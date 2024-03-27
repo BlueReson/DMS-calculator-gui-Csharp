cd26125a6e03504dbb28358cb389efe97fb0437a
Luke <luke.blw@gmail.com>
2024-03-27 09:46:12 +0100

    Add project files.

---
+++ DMS-calculator-C#/Form1.cs
@@ -0,0 +1,15 @@
+namespace DMS_calculator_C_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
